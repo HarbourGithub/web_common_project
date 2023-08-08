8b68ba0bf820bfc18d2cb485ab2c6af1c7b4921b
hgwang <hgwang@grandstream.cn>
2023-08-08 16:20:43 +0800

    [NBF] add code Cause: None Solution: None Fixed Version: None Branch: master

--- redux/reduxToolkitDemo.js
+++ redux/reduxToolkitDemo.js
@@ -6,28 +6,136 @@ const minusCount = document.getElementsByClassName('minus-count')
 const showUserInfo = document.getElementsByClassName('show-user-info')
 const changeUserName = document.getElementsByClassName('change-user-name')
 const changeUserAge = document.getElementsByClassName('change-user-age')
+const asyncChangeUserName = document.getElementsByClassName('async-change-user-name')
 
-const countReducer = (state = 0, action) => {
-    const { type, payload } = action
-    switch (type) {
-        case 'incrementCount':
-            return state + payload
-        case 'decrementCount':
-            return state - payload
-        default:
+const incrementCount = ReduxToolkit.createAction('INCREMENT_COUNT')
+const decrementCount = ReduxToolkit.createAction('DECREMENT_COUNT')
+
+const countReducer = ReduxToolkit.createReducer(0, (builder) => {
+    builder
+        .addCase(incrementCount, (state, action) => {
+            return state + action.payload
+        })
+        .addCase(decrementCount, (state, action) => {
+            return state - action.payload
+        })
+        .addMatcher((action) => action.payload === 100, (state, action) => {
+            return state + action.payload
+        })
+        .addDefaultCase((state, action) => {
             return state
+        })
+})
+
+// 模拟异步请求
+const simulateApiRequest = (value, delay) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(value)
+        }, delay)
+    })
+}
+
+// 创建异步thunk action
+const asyncChangeUserNameAction = ReduxToolkit.createAsyncThunk('asyncChangeUserName', async (value) => {
+    const response = await simulateApiRequest(value, 1000)
+    return response
+})
+
+const userInfoSlice = ReduxToolkit.createSlice({
+    name: 'userInfo',
+    initialState: { name: '张三', age: 18 },
+    reducers: {
+        changeUserNameAction(state, action) {
+            state.name = action.payload
+        },
+        changeUserAgeAction(state, action) {
+            state.age = action.payload
+        },
+        changeUserAgeAddTwo: {
+            reducer(state, action) {
+                state.age = action.payload
+            },
+            prepare(payload) {
+                payload = payload + 2
+                return { payload }
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(asyncChangeUserNameAction.pending, (state, action) => {
+                console.log('pending')
+            })
+            .addCase(asyncChangeUserNameAction.fulfilled, (state, action) => {
+                console.log('fulfilled')
+                state.name = action.payload
+            })
+            .addCase(asyncChangeUserNameAction.rejected, (state, action) => {
+                console.log('rejected')
+            })
     }
+})
+
+const { changeUserNameAction, changeUserAgeAction, changeUserAgeAddTwo } = userInfoSlice.actions
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    console.log('dispatching', action)
+    const result = next(action)
+    console.log('next state', store.getState())
+    return result
 }
 
 // 使用Redux Toolkit的configureStore方法创建store
 const store = ReduxToolkit.configureStore({
     reducer: {
-        counter: countReducer
+        count: countReducer,
+        userInfo: userInfoSlice.reducer
     },
+    middleware: [loggerMiddleware, ...ReduxToolkit.getDefaultMiddleware()]
 })
 
-const incrementCount = ReduxToolkit.createAction('incrementCount')
-const decrementCount = ReduxToolkit.createAction('decrementCount')
+const { dispatch, getState, subscribe } = store
 
+const assignCount = () => {
+    showCount[0].innerHTML = getState().count
+}
+const assignUserInfo = () => {
+    const userInfo = getState().userInfo
+    showUserInfo[0].innerHTML = `姓名：${userInfo.name}，年龄：${userInfo.age}`
+}
 
-console.log(incrementCount.toString(), decrementCount.toString())
+assignCount()
+assignUserInfo()
+
+subscribe(assignCount)
+subscribe(assignUserInfo)
+
+const boundActionsCreators = ReduxToolkit.bindActionCreators({
+    incrementCount,
+    decrementCount,
+    changeUserNameAction,
+    changeUserAgeAction,
+    changeUserAgeAddTwo,
+    asyncChangeUserNameAction
+}, dispatch)
+
+addCount[0].addEventListener('click', () => {
+    boundActionsCreators.incrementCount(3)
+})
+
+minusCount[0].addEventListener('click', () => {
+    boundActionsCreators.decrementCount(2)
+})
+
+changeUserName[0].addEventListener('click', () => {
+    boundActionsCreators.changeUserNameAction('李四')
+})
+
+changeUserAge[0].addEventListener('click', () => {
+    boundActionsCreators.changeUserAgeAddTwo(20)
+})
+
+asyncChangeUserName[0].addEventListener('click', () => {
+    boundActionsCreators.asyncChangeUserNameAction('王五')
+})
